85a6cc91276f3d167d266656b832e266bbf1eb15
Nicolás Montiel <nicolasmontielf@gmail.com>
2023-03-18 17:59:28 -0300

    Responsive table

--- src/components/PlatformList.tsx
+++ src/components/PlatformList.tsx
@@ -15,30 +15,32 @@ export function PlatformList({ results }: Props) {
     return (
         <>
             <h3 className="is-size-5">You can see {results.name} on:</h3>
-            <table className="table is-fullwidth mt-5">
-                <thead>
-                    <tr>
-                        <th>Platform</th>
-                        <th>Quality</th>
-                        <th>Link</th>
-                    </tr>
-                </thead>
-                <tbody className="has-text-left">
-                    {
-                        results.data.map((item: GetTitleOffersFlatrateResponse) => {
-                            return (
-                                <tr>
-                                    <td>{item.package.clearName}</td>
-                                    <td>{item.presentationType}</td>
-                                    <td>
-                                        <a href={item.standardWebURL} target="_blank">Watch it!</a>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>      
-            </table>
+            <div class="table-container mt-5">
+                <table className="table is-fullwidth">
+                    <thead>
+                        <tr>
+                            <th>Platform</th>
+                            <th>Quality</th>
+                            <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody className="has-text-left">
+                        {
+                            results.data.map((item: GetTitleOffersFlatrateResponse) => {
+                                return (
+                                    <tr>
+                                        <td>{item.package.clearName}</td>
+                                        <td>{item.presentationType}</td>
+                                        <td>
+                                            <a href={item.standardWebURL} target="_blank">Watch it!</a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>      
+                </table>
+            </div>
         </>
     )
 }
